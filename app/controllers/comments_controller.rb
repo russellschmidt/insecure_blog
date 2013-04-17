38b0346b7182ee93247fcadd60f545a40af90b57
Brendan Loudermilk <brendan@gophilosophie.com>
2013-04-17 16:26:04 -0700

    Improvements

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,23 @@
 class CommentsController < ApplicationController
+  before_filter :find_post
+
   def new
     @comment = Comment.new
   end
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params[:comment])
 
     if @comment.save
-      redirect_to @post
+      redirect_to @post, notice: "Your comment has been saved and is awaiting review by our staff"
     else
       render action: :new
     end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 end
